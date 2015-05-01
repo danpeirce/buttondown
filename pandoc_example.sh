c777cefda54aecea65a5325d41aab32726b62faf
Dan Peirce <peirce.dan@gmail.com>
2015-05-01 00:56:41 -0700

    changed sh example so --to=html5

--- pandoc_example.sh
+++ pandoc_example.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pandoc --from=markdown --to=html --standalone --smart --section-divs --css=pandocbd.css --output=pandoc_example.html pandoc_example.md
+pandoc --from=markdown --to=html5 --standalone --smart --section-divs --css=pandocbd.css --output=pandoc_example.html pandoc_example.md
